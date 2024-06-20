c978f83d92e0caa31c90a04a45ec06a635a75d2c
Miroslav Gannoha <miroslav.gannoha@gmail.com>
2024-06-20 21:08:28 +0300

    infinite 2-way parralax

--- Assets/Scripts/BackgroundParalaxUI.cs
+++ Assets/Scripts/BackgroundParalaxUI.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BackgroundParalaxUI : MonoBehaviour
 {
     public Transform playerCam;
-    public RectTransform[] backgrounds;
+    public RectTransform[] layers;
     public float[] offsetMultipliers;
 
     private Vector2 lastPos = Vector2.zero;
@@ -17,7 +17,7 @@ public class BackgroundParalaxUI : MonoBehaviour
     void Update()
     {
         updateDelta();
-        moveBackgrounds();
+        moveBackgroundLayers();
     }
 
     private void updateDelta()
@@ -27,19 +27,61 @@ public class BackgroundParalaxUI : MonoBehaviour
         lastPos = playerCam.position;
     }
 
-    private void moveBackgrounds()
+    private void moveBackgroundLayers()
     {
-        for (int i = 0; i < backgrounds.Length; ++i)
+        if (delta.x == 0)
+        {
+            return;
+        }
+        bool isGoingForward = delta.x > 0;
+        bool isGoingBackwards = delta.x < 0;
+        for (int i = 0; i < layers.Length; ++i)
         {
             float offsetMultiplier = offsetMultipliers.Length > i ? offsetMultipliers[i] : 1.0f;
-            for (int j = 0; j < backgrounds[i].childCount; ++j)
+            RectTransform lastChild = null;
+            RectTransform firstChild = null;
+            for (int j = 0; j < layers[i].childCount; ++j)
             {
-                var child = backgrounds[i].GetChild(j);
-                var childTransform = child.GetComponent<RectTransform>();
-                childTransform.anchoredPosition = new Vector2(
-                    childTransform.anchoredPosition.x - delta.x * offsetMultiplier,
-                    childTransform.anchoredPosition.y
-                );
+                var childTransform = layers[i].GetChild(j).GetComponent<RectTransform>();
+                if (lastChild == null)
+                {
+                    lastChild = childTransform;
+                }
+                else if (childTransform.anchorMax.x > lastChild.anchorMax.x)
+                {
+                    lastChild = childTransform;
+                }
+
+                if (firstChild == null)
+                {
+                    firstChild = childTransform;
+                }
+                else if (childTransform.anchorMin.x < firstChild.anchorMin.x)
+                {
+                    firstChild = childTransform;
+                }
+            }
+
+            for (int j = 0; j < layers[i].childCount; ++j)
+            {
+                var childTransform = layers[i].GetChild(j).GetComponent<RectTransform>();
+                float minX = childTransform.anchorMin.x;
+                float maxX = childTransform.anchorMax.x;
+                if (isGoingForward && childTransform.anchorMax.x < 0)
+                {
+                    var childDelta = childTransform.anchorMax.x - childTransform.anchorMin.x;
+                    minX = lastChild.anchorMax.x;
+                    maxX = lastChild.anchorMax.x + childDelta;
+                }
+                else if (isGoingBackwards && childTransform.anchorMin.x > 1)
+                {
+                    var childDelta = childTransform.anchorMax.x - childTransform.anchorMin.x;
+                    maxX = firstChild.anchorMin.x;
+                    minX = firstChild.anchorMin.x - childDelta;
+                }
+                float offset = delta.x * offsetMultiplier;
+                childTransform.anchorMin = new Vector2(minX - offset, childTransform.anchorMin.y);
+                childTransform.anchorMax = new Vector2(maxX - offset, childTransform.anchorMax.y);
             }
         }
     }
